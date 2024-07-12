4640447fd080f38d5e5e02ea1eb34992be4a5521
sm1lla <foxsmilla@gmail.com>
2024-07-12 18:07:01 +0200

    1.8.2 Show all existing species

--- src/main/java/com/opstty/AppDriver.java
+++ src/main/java/com/opstty/AppDriver.java
@@ -1,6 +1,7 @@
 package com.opstty;
 
 import com.opstty.job.DistrictList;
+import com.opstty.job.SpeciesList;
 import com.opstty.job.WordCount;
 import org.apache.hadoop.util.ProgramDriver;
 
@@ -13,7 +14,9 @@ public class AppDriver {
             programDriver.addClass("wordcount", WordCount.class,
                     "A map/reduce program that counts the words in the input files.");
             programDriver.addClass("districtlist", DistrictList.class,
-                    "A map/reduct program that lists all districs with trees.");
+                    "A map/reduce program that lists all districs with trees.");
+            programDriver.addClass("species", SpeciesList.class,
+                    "A map/reduce program that lists all species of trees.");
             exitCode = programDriver.run(argv);
         } catch (Throwable throwable) {
             throwable.printStackTrace();
---
+++ src/main/java/com/opstty/mapper/SpeciesMapper.java
@@ -0,0 +1,26 @@
+package com.opstty.mapper;
+
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class SpeciesMapper extends Mapper<Object, Text, Text, NullWritable> {
+    private boolean isFirstLine = true;
+
+    protected void map(Object key, Text value, Context context)
+            throws IOException, InterruptedException {
+        if (isFirstLine) {
+            isFirstLine = false; // Skip the first line
+            return;
+        }
+
+        String line = value.toString();
+        String[] columns = line.split(";"); // Assuming columns are space-separated
+        if (columns.length >= 4) {
+            context.write(new Text(columns[3]), NullWritable.get());
+        }
+    }
+
+}
