2d18e93a0071710900f98a6afd8faaa23cba144f
sm1lla <foxsmilla@gmail.com>
2024-07-12 17:41:50 +0200

    Ex 1.8.1 Districts containing trees

--- src/main/java/com/opstty/AppDriver.java
+++ src/main/java/com/opstty/AppDriver.java
@@ -1,5 +1,6 @@
 package com.opstty;
 
+import com.opstty.job.DistrictList;
 import com.opstty.job.WordCount;
 import org.apache.hadoop.util.ProgramDriver;
 
@@ -11,7 +12,8 @@ public class AppDriver {
         try {
             programDriver.addClass("wordcount", WordCount.class,
                     "A map/reduce program that counts the words in the input files.");
-
+            programDriver.addClass("districtlist", DistrictList.class,
+                    "A map/reduct program that lists all districs with trees.");
             exitCode = programDriver.run(argv);
         } catch (Throwable throwable) {
             throwable.printStackTrace();
